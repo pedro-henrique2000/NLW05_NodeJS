46c5f68fbf8a6dfefb54a470b3f2fd02b3b8a72a
phb2000 <p.becker2000@hotmail.com>
2021-04-20 15:15:24 -0300

    second day completed

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,12 @@
 import express from "express";
 
+import "./database";
+import { routes } from "./routes";
+
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({
-    message: "Hello World!",
-  });
-});
+app.use(express.json());
 
-app.post("/", (request, response) => {
-  return response.json({
-    message: "cadastro realizado com sucesso",
-  });
-});
+app.use(routes);
 
 app.listen(3333, () => console.log("Aplicação Rodando!"));
